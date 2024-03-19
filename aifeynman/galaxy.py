1612bc3c3c1b0c431008df40f1eca03fe737cf05
Clan328 <dominysan@gmail.com>
2024-03-19 13:11:45 +0100

    Updated to include final code and ee

--- aifeynman/galaxy.py
+++
@@ -1,8 +0,0 @@
-class Galaxy:
-    def __init__(self, M, SM, SFR):
-        self.M = M
-        self.SM = SM
-        self.SFR = SFR
-        
-    def __repr__(self):
-        return f"<Galaxy M:{self.M} SM:{self.SM} SFR:{self.SFR}>"
